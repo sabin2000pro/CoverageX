1bbffbb42e778ee38d590b36b8caa675d5ce23c2
sabin2000pro <sabinlungu293@gmail.com>
2023-07-29 19:45:26 +0100

    Creating the root route for auth service

--- backend/services/authentication/src/index.ts
+++ backend/services/authentication/src/index.ts
@@ -1,4 +1,11 @@
+import dotenv from 'dotenv';
+dotenv.config({path: '.env'});
+
 import express from 'express'
 import morgan from 'morgan';
 import xss from 'xss-clean';
 import cors from 'cors';
+import mongoSanitize from 'express-mongo-sanitize'
+
+const app: any = express();
+const AUTH_SERVICE_PORT: string = process.env.AUTH_SERVICE_PORT || ""
