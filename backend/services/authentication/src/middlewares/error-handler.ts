d0e26a8587136421f9547c186bef487265ae4c82
sabin2000pro <sabinlungu293@gmail.com>
2023-07-29 20:59:21 +0100

    Created custom error handler for the auth microservice

--- backend/services/authentication/src/middlewares/error-handler.ts
+++ backend/services/authentication/src/middlewares/error-handler.ts
@@ -1,4 +1,7 @@
+import { ErrorResponse } from "../utils/error-response"
 
-export const errorHandler = (err, request, response, next) => {
-    
+export const errorHandler = (err: any, request: any, response: any, next: any) => {
+    let error = {...err}
+
+    return response.status()
 }
