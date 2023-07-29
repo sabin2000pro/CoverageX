8af3aac819dc4877af8bb09ac21866c5686e2edc
sabin2000pro <sabinlungu293@gmail.com>
2023-07-29 20:05:32 +0100

    Configuring the analytics microservice & installing dependencies

---
+++ backend/services/analytics/src/interfaces/analytics-interface.ts
@@ -0,0 +1,4 @@
+
+export interface IAnalytics {
+    totalPolicies: number
+}
--- backend/services/authentication/src/controllers/auth-controllers.ts
+++ backend/services/authentication/src/controllers/auth-controllers.ts
@@ -6,3 +6,16 @@ export const registerUser = async (request: any, response: any, next: any): Prom
 export const loginUser = async (request: any, response: any, next: any): Promise<any> => {
     
 }
+
+export const forgotPassword = async (request: any, response: any, next: any): Promise<any> => {
+
+}
+
+export const resetPassword = async (request: any, response: any, next: any): Promise<any> => {
+
+}
+
+export const getLoggedInUser = async (request: any, response: any, next: any): Promise<any> => {
+
+}
+
