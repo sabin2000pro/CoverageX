8b07ff8a2879ffa011a06b7cf20e3aebefb88e7e
sabin2000pro <sabinlungu293@gmail.com>
2023-07-29 21:16:05 +0100

    Establishing database connection to auth service

---
+++ backend/services/authentication/src/database/connect-db.ts
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const AUTH_DB_URI = process.env.AUTH_DB_URI || ""
+
+export const connectAuthSchema = async () => {
+    try {
+
+      const dbConnection = await mongoose.connect(AUTH_DB_URI);
+
+      if(dbConnection.connection) {
+         console.log(`Connected successfully to auth database...`);
+      }
+
+      else {
+        console.log(`Could not connect to auth database`)
+      }
+
+
+    } 
+    
+    catch(error) {
+
+      if(error) {
+        
+        throw new Error(error);
+      }
+
+
+    }
+
+}
