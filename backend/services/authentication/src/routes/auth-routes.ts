ffc6189b654a150ce8a57cc5be727956ecf0aefd
sabin2000pro <sabinlungu293@gmail.com>
2023-07-30 11:41:53 +0100

    Mounting the auth routes

--- backend/services/authentication/src/routes/auth-routes.ts
+++ backend/services/authentication/src/routes/auth-routes.ts
@@ -5,9 +5,10 @@ const authRouter: any = express.Router();
 
 // Mount the routes
 authRouter.route('/register').post(registerUser);
-authRouter.route('/login').post(loginUser);
+authRouter.route('/login').post(loginUser as any);
 authRouter.route('/verify-email').post(verifyEmailAddress)
 authRouter.route('/verify-login-mfa').post(verifyLoginMFA)
 authRouter.route('/forgot-password').post(forgotPassword)
+authRouter.route('/reset-password').post(resetPassword);
 
 export {authRouter}
