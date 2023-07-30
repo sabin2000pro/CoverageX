4f226dde9d7969986d725754dc020107ca2646a8
sabin2000pro <sabinlungu293@gmail.com>
2023-07-30 18:10:20 +0100

    Completed the policy data model

---
+++ backend/services/policies/src/model/policy-model.ts
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose'
+
+export const PolicySchema = new mongoose.Schema({
+
+    policyNumber: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    coverageType: {
+        type: String,
+        required: true,
+    },
+
+    startDate: {
+        type: Date,
+        default: Date.now
+    },
+
+    endDate: {
+        type: Date,
+        default: Date.now
+    },
+
+    premiumAmount: {
+        type: Number,
+        required: true,
+    },
+
+    insuredCar: {
+        make: String,
+        model: String,
+        year: String,
+        registrationNumber: String
+    },
+
+    holder: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+      }
+
+})
+
+const Policy = mongoose.model("Policy", PolicySchema);
+export {Policy}
