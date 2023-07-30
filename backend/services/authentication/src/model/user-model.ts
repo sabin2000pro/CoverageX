127352f4b98acc71f94434ab2bc7146e870faf7e
sabin2000pro <sabinlungu293@gmail.com>
2023-07-30 09:46:34 +0100

    Working on the user data model and adding validation to the attributes

--- backend/services/authentication/src/model/user-model.ts
+++ backend/services/authentication/src/model/user-model.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcryptjs'
 
 export const UserSchema = new mongoose.Schema({
 
@@ -8,14 +9,49 @@ export const UserSchema = new mongoose.Schema({
     },
 
     email: {
+        type: String,
+        required: [true, "Please provide a valid e-mail address for the user"],
+        unique: true,
+        
+        validate: {
 
+            validator: function(val) {
+                const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+                return emailRegex.test(val);
+            },
+
+            message: "Please provide a valid e-mail address"
+
+        }
     },
     
     password: {
+        type: String,
+        unique: true,
+        select: false,
+        required: [true, "Please provide a valid password for the user"]
+    },
 
+    gender: {
+        type: String,
+        required: [true, "Please specify the users gender"],
+        enum: ['male', 'female', 'non-binary']
+    },
+
+    role: {
+        type: String,
+        enum: ['regular-user', 'admin', 'agent', 'policyholder', 'claims-adjuster']
     }
 
 }, {timestamps: true} )
+
+UserSchema.pre('save', async function(next) {
+
+})
+
+UserSchema.methods.compareLoginPasswords = async function(enteredPassword: string): Promise<boolean> {
+    return false;
+}
 
 const User = mongoose.model("User", UserSchema);
 export {User}
